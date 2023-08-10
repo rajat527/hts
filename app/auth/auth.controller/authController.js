0ca020620ca5970549af6e77131d82397e7c87fc
Rajat Chauhan <61515514+rajat527@users.noreply.github.com>
2023-08-10 08:46:18 +0530

    main

--- app/auth/auth.controller/authController.js
+++ app/auth/auth.controller/authController.js
@@ -145,7 +145,244 @@ auth.login = async (req, res) => {
     }
 }
 
+/*
 
+let arr = [
+  {
+    name: "rajat",
+    products: [
+      {
+        title: "product1",
+        stateId: 1
+      },
+      {
+        title: "product2",
+        stateId: 2
+      },
+      {
+        title: "product2",
+        stateId: 1
+      }
+    ],
+    stateId: 2
+  },
+
+  {
+    name: "shivam",
+    products: [
+      {
+        title: "pr1",
+        stateId: 2
+      },
+      {
+        title: "pr2",
+        stateId: 2
+      },
+      {
+        title: "pr3",
+        stateId: 2
+      }
+    ],
+    stateId: 1
+  },
+  {
+    name: "ttrt",
+    products: [
+      {
+        title: "pr001",
+        stateId: 3
+      },
+      {
+        title: "pr222",
+        stateId: 3
+      },
+      {
+        title: "pr333",
+        stateId: 2
+      }
+    ],
+    stateId: 8
+  },
+];
+
+for (let i = 0; i < arr.length; i++) {
+  const products = arr[i].products;
+  const productStateIds = products.map(product => product.stateId);
+  
+  if (productStateIds.every(stateId => stateId === 1)) {
+    arr[i].stateId = 3;
+  } else if (productStateIds.every(stateId => stateId === 2)) {
+    arr[i].stateId = 1;
+  } else if (productStateIds.every(stateId => stateId === 3)) {
+    arr[i].stateId = 1;
+  }
+}
+
+console.log(arr);
+
+
+
+*/
+
+
+
+
+/*
+let arr = [
+  {
+    name: "rajat",
+    products: [
+      {
+        title: "product1",
+        stateId: 1
+      },
+      {
+        title: "product2",
+        stateId: 2
+      },
+      {
+        title: "product2",
+        stateId: 2
+      }
+    ],
+    stateId: 3
+  },
+
+  {
+    name: "shivam",
+    products: [
+      {
+        title: "pr1",
+        stateId: 2
+      },
+      {
+        title: "pr2",
+        stateId: 2
+      },
+      {
+        title: "pr3",
+        stateId: 2
+      }
+    ],
+    stateId: 1
+  },
+  {
+    name: "tolla",
+    products: [
+      {
+        title: "clo1",
+        stateId: 4
+      },
+      {
+        title: "clo2",
+        stateId: 4
+      },
+      {
+        title: "clo3",
+        stateId: 4
+      }
+    ],
+    stateId: 1
+  },
+];
+
+for (let i = 0; i < arr.length; i++) {
+  const products = arr[i].products;
+  const firstProductId = products[0].stateId;
+  let allSame = true;
+
+  for (let j = 1; j < products.length; j++) {
+    if (products[j].stateId !== firstProductId) {
+      allSame = false;
+      break;
+    }
+  }
+
+  if (allSame) {
+    arr[i].stateId = firstProductId;
+  }
+}
+
+console.log(arr);
+*/
+
+
+/*
+
+let arr1 = [
+  {
+    id: 1,
+    name: "rajat",
+    products: {
+      title: "product1",
+      stateId: 1
+    },
+    stateId: 3
+  },
+  {
+    id: 1,
+    name: "rajat",
+    products: {
+      title: "product2",
+      stateId: 1
+    },
+    stateId: 3
+  },
+  {
+    id: 1,
+    name: "rajat",
+    products: {
+      title: "product3",
+      stateId: 1
+    },
+    stateId: 3
+  },
+  {
+    id: 2,
+    name: "rajat2",
+    products: {
+      title: "product33",
+      stateId: 1
+    },
+    stateId: 2
+  }
+];
+
+let combinedArr = [];
+
+for (let i = 0; i < arr1.length; i++) {
+  const existingItem = combinedArr.find(item => item.id === arr1[i].id && item.name === arr1[i].name);
+
+  if (existingItem) {
+    existingItem.products.push(arr1[i].products);
+  } else {
+    const newItem = {
+      id: arr1[i].id,
+      name: arr1[i].name,
+      products: [arr1[i].products],
+      stateId: arr1[i].stateId
+    };
+    combinedArr.push(newItem);
+  }
+}
+
+for (let i = 0; i < combinedArr.length; i++) {
+  const products = combinedArr[i].products;
+  const productStateIds = products.map(product => product.stateId);
+
+  if (productStateIds.every(stateId => stateId === 1)) {
+    combinedArr[i].stateId = 3;
+  } else if (productStateIds.every(stateId => stateId === 2)) {
+    combinedArr[i].stateId = 1;
+  } else if (productStateIds.every(stateId => stateId === 3)) {
+    combinedArr[i].stateId = 1;
+  }
+}
+
+console.log(combinedArr);
+
+
+*/
 
 
 module.exports = auth
